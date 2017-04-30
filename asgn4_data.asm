a680aa196299ad5b058e36e8b9cfb9e41f4d2a5a
AlecWilliams <alecvwilliams@hotmail.com>
2017-04-30 16:53:08 -0700

    Upload of basic working quiz. Does nothing with results currently

---
+++ asgn4_data.asm
@@ -0,0 +1,28 @@
+	 .ORIG 	x3300
+	
+	;Are you Julie Workman? Take this short quiz to find out!!
+
+Q1        .STRINGZ  "\nWhat is your favorite alcohol?\n   1 - Vodka\n   2 - Gin\n   3 - Whiskey\n   4 - Tequila\n"
+Q1Answ   .FILL    #8
+         .FILL    #2
+         .FILL    #12
+         .FILL    #3
+
+Q2    	 .STRINGZ  "\nWhat is your favorite programming language?\n   1 - Javascript\n   2 - PHP\n   3 - Perl\n   4 - C\n"
+Q2Answ   .FILL    #1
+         .FILL    #4
+         .FILL    #3
+         .FILL    #8
+
+Q3    	 .STRINGZ  "\nWhat is your favorite type of shirt to wear?\n   1 - T-shirts about programming\n   2 - Dress shirts\n   3 - Polos\n   4 - Tank-tops\n"
+Q3Answ   .FILL    #10
+         .FILL    #3
+         .FILL    #6
+         .FILL    #5
+
+Result1	.STRINGZ  "\nYou are 100% not Julie Workman."
+Result2	.STRINGZ  "\nYou are most likely not Julie. "
+Result3	.STRINGZ  "\nHmm, theres a good chance you are Julie."
+Result4	.STRINGZ  "\You are Definitely Julie. Hi Julie!!"
+
+	.END
